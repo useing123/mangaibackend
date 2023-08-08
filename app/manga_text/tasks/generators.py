573859ad4e52def43da4e1f4b0497e16a0b48e0e
useing123 <useing@mail.ru>
2023-08-08 18:05:33 +0600

    added new model

--- app/manga_text/tasks/generators.py
+++ app/manga_text/tasks/generators.py
@@ -264,7 +264,7 @@ def generate_image(manga_id: str, manga_images_description: str, repository: Man
 
     for frame in frames:
         # Call the Replicate API to generate an image
-        model_version = "cjwbw/anything-v3-better-vae:09a5805203f4c12da649ec1923bb7729517ca25fcac790e640eaa9ed66573b65"
+        model_version = "stability-ai/sdxl:2b017d9b67edd2ee1401238df49d75da53c523f36e363881e057f5dc3ed3c5b2"
         inputs = {"prompt": frame}
 
         # We need to set the token environment variable for the replicate.run function
@@ -293,6 +293,7 @@ def generate_image(manga_id: str, manga_images_description: str, repository: Man
 
         # Add the Imgur link to the list
         imgur_links.append(response.json()["data"]["link"])
+        
 
     # Update manga information with the Imgur links in the repository
     repository.update_manga(manga_id, {"imgur_links": imgur_links})
